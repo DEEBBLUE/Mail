a77b457c412f9222a0898ba50b670cbcd70aed7c
Lifter <Sosite.my_dick@gmail.com>
2025-06-16 15:23:27 -0400

    Create auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 const Auth: FC = () => {
   return (
-    <div>Hwwll</div>
+    <div className="size-[200px] border-[2px] border-black m-auto"></div>
   ) 
 }
 export default Auth;
