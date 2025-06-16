47d50e3461c16f7a6f46180eaec59588767928de
Lifter <Sosite.my_dick@gmail.com>
2025-06-16 18:13:06 -0400

    Add styte of auth: login and registration

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,29 +1,47 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
+
+type Auth = "reg" | "login";
 
 const Auth: FC = () => {
+  const [authState, setAuthState] = useState<Auth>("login");
   return (
     <main>
       <section className="w-full h-screen flex justify-center items-center">
-        <form className="p-[20px] border-[2px] border-black flex flex-col">
-          <label className="flex flex-col">
-            Login
-            <input 
-              className="w-[325px] h-[60px] border-[2px] border-black"
-              type="text"
-            />
-          </label>
-          <label className=" flex flex-col">
-            Password
-            <input 
-              className="w-[325px] h-[60px] border-[2px] border-black"
-              type="text"
-            />
-          </label>
-          <button className="bg-black w-[325px] h-[60px] mt-[10px] text-white" type="submit">LogIn</button>
-          <button className="text-[12px] mt-[10px]">Registration</button>
-        </form>
+        <div className="p-[20px] border-[2px] border-black flex flex-col">
+          <form>
+            <label className="flex flex-col">
+              Login
+              <input
+                className="w-[325px] h-[60px] border-[2px] border-black"
+                type="text"
+              />
+            </label>
+            <label className=" flex flex-col mt-[5px]">
+              Password
+              <input
+                className="w-[325px] h-[60px] border-[2px] border-black"
+                type="text"
+              />
+            </label>
+            <button
+              className="bg-black w-[325px] h-[60px] mt-[10px] text-white"
+              type="submit"
+            >
+              {authState === "login" ? "LogIn" : "Registration"}
+            </button>
+          </form>
+          <button
+            className=" text-[12px] mt-[10px]"
+            onClick={() => {
+              authState === "login" ? setAuthState("reg") : setAuthState("login")
+            }}
+          >
+            {authState === "login" ? "Registration" : "LogIn"}
+          </button>
+        </div>
       </section>
     </main>
-  ) 
-}
+  );
+};
 export default Auth;
