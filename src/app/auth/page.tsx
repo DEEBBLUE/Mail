9d74c202e92971aec9ff507152331c49b06a7968
Lifter <Sosite.my_dick@gmail.com>
2025-06-16 17:44:08 -0400

    Made up LogIn page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,29 @@
 import { FC } from "react";
 
-
 const Auth: FC = () => {
   return (
-    <div className="size-[200px] border-[2px] border-black m-auto"></div>
+    <main>
+      <section className="w-full h-screen flex justify-center items-center">
+        <form className="p-[20px] border-[2px] border-black flex flex-col">
+          <label className="flex flex-col">
+            Login
+            <input 
+              className="w-[325px] h-[60px] border-[2px] border-black"
+              type="text"
+            />
+          </label>
+          <label className=" flex flex-col">
+            Password
+            <input 
+              className="w-[325px] h-[60px] border-[2px] border-black"
+              type="text"
+            />
+          </label>
+          <button className="bg-black w-[325px] h-[60px] mt-[10px] text-white" type="submit">LogIn</button>
+          <button className="text-[12px] mt-[10px]">Registration</button>
+        </form>
+      </section>
+    </main>
   ) 
 }
 export default Auth;
