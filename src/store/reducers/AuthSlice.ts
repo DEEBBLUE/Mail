775c66bbf9a8dbde3eb631e4ebfa804a2de50fe9
Lifter <Sosite.my_dick@gmail.com>
2025-06-17 06:03:34 -0400

    Create Auth slice

---
+++ src/store/reducers/AuthSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface AuthState{
+  isAuth: boolean
+}
+
+const initialState: AuthState = {
+  isAuth: false 
+}
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuth(state,payload: PayloadAction<boolean>){
+      state.isAuth = payload.payload
+    }
+  }
+}) 
+
+export default authSlice.reducer
