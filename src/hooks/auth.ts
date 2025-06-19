0bf3503f174cb35c470edf0d722b1cd74642d80b
Lifter <Sosite.my_dick@gmail.com>
2025-06-19 17:42:00 -0400

    Create hooks for auth

---
+++ src/hooks/auth.ts
@@ -0,0 +1,19 @@
+import { IAuthResponse } from "@/models/auth.models";
+import axios, { AxiosResponse } from "axios";
+
+const baseUrl = "http://localhost:5000"
+
+export function useReg(login: string,password: string): Promise<AxiosResponse<IAuthResponse>> {
+  return axios.post(`${baseUrl}/reg`,{
+    login: login,
+    passwd: password
+  }) 
+}
+
+export function useLogin(login: string,password: string):Promise<AxiosResponse<IAuthResponse>> {
+  return axios.post(`${baseUrl}/login`,{
+    login: login,
+    passwd: password
+  }) 
+}
+
